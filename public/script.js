da5bf6753b01ce628918f55d4a8485375387eca4
Bryce Pavay <pavayb@yahoo.com>
2020-11-09 02:16:04 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -24,7 +24,7 @@ const html = matchArray.map(place => {
   const categoryName = place.category.replace(regex, `<span class="hl">${this.value}</span>`);
   const address_line_1Name = place.address_line_1.replace(regex,`<span class="hl">${this.value}</span>`);
   return `
-  <ul class="suggestions>
+  
     <li>
       <span class="name">${restarauntName}</span>
       <br>
@@ -32,7 +32,7 @@ const html = matchArray.map(place => {
       <br>
       <span class="address_line_1Name">${address_line_1Name}</span>
     </li>
-    </ul>
+   
   `;
   
 }).join('');
