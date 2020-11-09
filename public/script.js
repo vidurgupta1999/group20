d23e5e9e434ac655fca427fae9892291e0909cc9
Bryce Pavay <pavayb@yahoo.com>
2020-11-09 03:20:06 -0500

    commit latest

--- public/script.js
+++ public/script.js
@@ -1,38 +1,54 @@
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 const restaraunt = [];
 fetch(endpoint)
-.then(blob => blob.json())
-.then(data => restaraunt.push(...data));
+    .then(blob => blob.json())
+    .then(data => restaraunt.push(...data));
 
 function findMatches(wordToMatch, restaraunt) {
-return restaraunt.filter(place => {
-  // here we need to figure out if the city or state matches what was searched
-  const regex = new RegExp(wordToMatch, 'gi');
-  return place.name.match(regex) || place.category.match(regex)
-});
+    return restaraunt.filter(place => {
+        // here we need to figure out if the city or state matches what was searched
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.name.match(regex) || place.category.match(regex)
+    });
 }
-if (document.GetElementById('searchInput').value == '')
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "block";
-}
+function search() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("restaraunts");
+    li = ul.getElementsByTagName('li');
+
+    if (input.value.length == 0) {
+        ul.style.display = "none";
+        return;
     } else {
-        li[i].style.display = "none";
+        ul.style.display = "block";
+
+        if (document.GetElementById('searchInput').value == '')
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("a")[0];
+                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "block";
+                }
+            } else {
+            li[i].style.display = "none";
+        }
+    }
 }
 
 function numberWithCommas(x) {
-return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function displayMatches() {
-const matchArray = findMatches(this.value, restaraunt);
-const html = matchArray.map(place => {
-  const regex = new RegExp(this.value, 'gi');
-  const restarauntName = place.name.replace(regex, `<span class="hl">${this.value}</span>`);
-  const categoryName = place.category.replace(regex, `<span class="hl">${this.value}</span>`);
-  const address_line_1Name = place.address_line_1.replace(regex,`<span class="hl">${this.value}</span>`);
-  return `
+    const matchArray = findMatches(this.value, restaraunt);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const restarauntName = place.name.replace(regex, `<span class="hl">${this.value}</span>`);
+        const categoryName = place.category.replace(regex, `<span class="hl">${this.value}</span>`);
+        const address_line_1Name = place.address_line_1.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
   
     <li> 
       <span class="name">${restarauntName}</span>
@@ -43,16 +59,14 @@ const html = matchArray.map(place => {
     </li>
    
   `;
-  
-}).join('');
-suggestions.innerHTML = html;
-}   
+
+    }).join('');
+    suggestions.innerHTML = html;
+}
 
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
-
-
 
